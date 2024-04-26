06e2eafe43a2737c8d3cd72117458b0a5276e14d
Rayhanmz02 <raimuhzik69@gmail.com>
2024-04-26 10:48:19 +0700

    done

--- app/src/main/java/com/example/utspb/data/retrofit/ApiService.java
+++ app/src/main/java/com/example/utspb/data/retrofit/ApiService.java
@@ -11,11 +11,11 @@ import retrofit2.http.Query;
 
 public interface ApiService {
 
-    @Headers({"Authorization: token "})
+
     @GET("search/users")
     Call<GithubSearch> searchUsers(@Query("q") String query);
 
-    @Headers({"Authorization: token "})
+
     @GET("users/{username}")
     Call<Users> getUser(@Path("username") String username);
 
